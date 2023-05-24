bfab726fda64e8a99d8c50147967653643c348ea
Haider Qaizar Hussain <haider_qaizar.hussain@dfki.de>
2023-05-25 00:08:11 +0200

    safe_dist updated

--- scripts/safe_distance.py
+++ scripts/safe_distance.py
@@ -11,14 +11,11 @@ import cv2
 FOCAL_LENGTH = 190
 
 
-# function to find the duckiebot (assume largest object in image)
 def find_marker(image):
 	# convert the image to grayscale, blur it, and detect edges
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray, (5, 5), 0)
 	edged = cv2.Canny(gray, 35, 125)
-	# find the contours in the edged image and keep the largest one;
-	# we'll assume that this is our duckiebot in the image
 	cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 	cnts = imutils.grab_contours(cnts)
 	c = max(cnts, key = cv2.contourArea)
